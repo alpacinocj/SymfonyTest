4830cd68a8dd5c280546974f4e9c675cceabd36c
chengjiang <279278253@qq.com>
2017-09-22 17:25:07 +0800

    新建Controller

--- src/Mary/WebBundle/Controller/DefaultController.php
+++ src/Mary/WebBundle/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace Mary\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('MaryWebBundle:Default:index.html.twig');
+    }
+
+    public function helloAction(Request $request, $name)
+    {
+        return $this->render('MaryWebBundle:Default:hello.html.twig', [
+            'name' => $name
+        ]);
     }
 }
