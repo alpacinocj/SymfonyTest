35d93545ed3ded4e1ca4245acfeb279a73be7fbe
chengjiang <279278253@qq.com>
2017-11-01 13:45:35 +0800

    删除默认AppBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Mary\WebBundle\MaryWebBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Mary\AdminBundle\MaryAdminBundle(),
