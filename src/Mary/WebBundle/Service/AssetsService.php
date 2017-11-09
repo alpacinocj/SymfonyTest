c18dfc5aa45b9e5b19bb498256d0ef053e25ffe7
chengjiang <279278253@qq.com>
2017-11-09 11:57:01 +0800

    AssetsService代码调整

--- src/Mary/WebBundle/Service/AssetsService.php
+++ src/Mary/WebBundle/Service/AssetsService.php
@@ -7,7 +7,7 @@ use Symfony\Component\Asset\UrlPackage;
 use Symfony\Component\Asset\PathPackage;
 use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
-use Symfony\Component\Asset\Exception\InvalidArgumentException;
+use JBZoo\Utils\Url as UrlUtil;
 
 class AssetsService extends BaseService
 {
@@ -22,11 +22,6 @@ class AssetsService extends BaseService
         $this->assetsVersion = defined('APP_ENV_PROD') && APP_ENV_PROD ? $assetsVersion : $this->_getRandAssetsVersion();
         $this->versionFormat = null;
         $this->assetsBasePath = null;
-    }
-
-    private function _getRandAssetsVersion()
-    {
-        return microtime(true);
     }
 
     public function setVersionFormat($format)
@@ -59,13 +54,8 @@ class AssetsService extends BaseService
     {
         $pathInfo = pathinfo($assetFile);
         $basePath = !empty($this->assetsBasePath) ? $this->assetsBasePath : $pathInfo['dirname'];
-        $parse = parse_url($assetFile);
-        if (isset($parse['scheme']) && isset($parse['host'])) {
-            // absolute asset
-            $package = $this->getUrlPackage($basePath, $this->assetsVersion, $this->versionFormat);
-        } else {
-            $package = $this->getPathPackage($basePath, $this->assetsVersion, $this->versionFormat);
-        }
+        $class = UrlUtil::isAbsolute($assetFile) ? 'getUrlPackage' : 'getPathPackage';
+        $package = $this->$class($basePath, $this->assetsVersion, $this->versionFormat);
         $path = $package->getUrl($pathInfo['basename']);
         $this->_reset();
         return $path;
@@ -75,6 +65,11 @@ class AssetsService extends BaseService
     {
         $this->versionFormat = null;
         $this->assetsBasePath = null;
+    }
+
+    private function _getRandAssetsVersion()
+    {
+        return microtime(true);
     }
 
 }
