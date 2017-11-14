4fe8506a4845957d4914b747ae526918058c8d27
chengjiang <279278253@qq.com>
2017-11-14 09:47:13 +0800

    Twig模板扩展返回安全的HTML标签

--- src/Mary/WebBundle/Twig/Extension/AssetsExtension.php
+++ src/Mary/WebBundle/Twig/Extension/AssetsExtension.php
@@ -15,11 +15,12 @@ class AssetsExtension extends \Twig_Extension
 
     public function getFunctions()
     {
+        $options = ['is_safe' => ['html']];
         return [
-            new \Twig_SimpleFunction('assetCss', [$this, 'assetCss']),
-            new \Twig_SimpleFunction('assetJs', [$this, 'assetJs']),
-            new \Twig_SimpleFunction('assetImg', [$this, 'assetImg']),
-            new \Twig_SimpleFunction('uploadedImg', [$this, 'uploadedImg']),
+            new \Twig_SimpleFunction('assetCss', [$this, 'assetCss'], $options),
+            new \Twig_SimpleFunction('assetJs', [$this, 'assetJs'], $options),
+            new \Twig_SimpleFunction('assetImg', [$this, 'assetImg'], $options),
+            new \Twig_SimpleFunction('uploadedImg', [$this, 'uploadedImg'], $options),
         ];
     }
 
@@ -28,8 +29,8 @@ class AssetsExtension extends \Twig_Extension
         if (null !== $basePath) {
             $this->assetsService->setAssetsBasePath($basePath);
         }
-        echo "<link rel=\"stylesheet\" href=\"{$this->assetsService->getAssetPath($cssFile)}\">";
-        return null;
+        $link = "<link rel=\"stylesheet\" href=\"{$this->assetsService->getAssetPath($cssFile)}\">";
+        return $link;
     }
 
     public function assetJs($jsFile, $basePath = null)
@@ -37,8 +38,8 @@ class AssetsExtension extends \Twig_Extension
         if (null !== $basePath) {
             $this->assetsService->setAssetsBasePath($basePath);
         }
-        echo "<script src=\"{$this->assetsService->getAssetPath($jsFile)}\"></script>";
-        return null;
+        $script = "<script src=\"{$this->assetsService->getAssetPath($jsFile)}\"></script>";
+        return $script;
     }
 
     public function assetImg($imgFile, $basePath = null, $width = 'auto', $height = 'auto')
@@ -46,8 +47,8 @@ class AssetsExtension extends \Twig_Extension
         if (null !== $basePath) {
             $this->assetsService->setAssetsBasePath($basePath);
         }
-        echo "<img src=\"{$this->assetsService->getAssetPath($imgFile)}\" alt=\"{$imgFile}\" width=\"{$width}\" height=\"{$height}\">";
-        return null;
+        $img = "<img src=\"{$this->assetsService->getAssetPath($imgFile)}\" alt=\"{$imgFile}\" width=\"{$width}\" height=\"{$height}\">";
+        return $img;
     }
 
     public function uploadedImg($targetFile, $width = 'auto', $height = 'auto')
