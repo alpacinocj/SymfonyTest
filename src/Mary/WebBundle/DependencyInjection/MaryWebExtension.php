6266c824db72dddbd8be77787a27765c659131a2
chengjiang <279278253@qq.com>
2017-11-17 12:00:23 +0800

    加载Bundle自定义配置并设置为容器参数

--- src/Mary/WebBundle/DependencyInjection/MaryWebExtension.php
+++ src/Mary/WebBundle/DependencyInjection/MaryWebExtension.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Mary\WebBundle\Service\AssetsService;
 use Mary\WebBundle\Service\UploaderService;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -26,6 +27,12 @@ class MaryWebExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        // 加载Bundle配置并设置为容器参数
+        if (!$container->hasParameter('maryweb.config')) {
+            $config = Yaml::parse(file_get_contents(APP_CONFIG_PATH . 'config_web.yml'));
+            $container->setParameter('maryweb.config', $config);
+        }
 
         // 添加Bundle常用类并编译, 减少I/O操作
         // 3.2新增 使用 patterns 来添加待编译类的选项自 Symfony 3.2 起被引入。
